c96218935babdb3d9014039d7d86406df1d9ad32
abenstirling <abenstirling@pm.me>
2023-02-16 20:36:04 -0800

    Swift updates

---
+++ Swift/AirPark/AirPark/BlinkyMOdel.swift
@@ -0,0 +1,118 @@
+//
+//  BlinkyModel.swift
+//  Blinkytooth
+//
+//  Created by Ben Stirling on 2/14/23.
+//
+
+import Foundation
+import CoreBluetooth
+import SwiftUI
+
+class BlinkyModel: NSObject, ObservableObject, CBPeripheralDelegate, CBCentralManagerDelegate {
+
+    //
+    var centralManager: CBCentralManager!
+    var peripheral: CBPeripheral?
+
+    // UUID for the service and characteristic, repsectively
+    let SERVICE_UUID = CBUUID(string: "4fafc201-1fb5-459e-8fcc-c5c9c331914b")
+    let CHARACTERISTIC_UUID = CBUUID(string: "beb5483e-36e1-4688-b7f5-ea07361b26a8")
+
+    var characteristic: CBCharacteristic?
+
+    // These inform the UI what state the bluetooth is in
+    @Published var connected: Bool = false
+    @Published var loaded: Bool = false
+    @Published var ledState: Bool = false
+
+    override init() {
+        super.init()
+        centralManager = CBCentralManager(delegate: self, queue: nil)
+    }
+
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        switch central.state {
+        case .poweredOff:
+            print("Is Powered Off.")
+        case .poweredOn:
+            print("Is Powered On.")
+            startScanning()
+        case .unsupported:
+            print("Is Unsupported.")
+        case .unauthorized:
+            print("Is Unauthorized.")
+        case .unknown:
+            print("Unknown")
+        case .resetting:
+            print("Resetting")
+        @unknown default:
+            print("Error")
+        }
+    }
+
+    func startScanning() {
+        centralManager.scanForPeripherals(withServices: [SERVICE_UUID])
+    }
+
+    func stopScanning() {
+        centralManager.stopScan()
+    }
+
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+
+        self.peripheral = peripheral
+        centralManager.connect(self.peripheral!)
+    }
+
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+
+        self.peripheral!.delegate = self
+        self.peripheral!.discoverServices([SERVICE_UUID])
+
+        stopScanning()
+
+        withAnimation {
+            connected = true
+        }
+    }
+
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        guard let services = peripheral.services else { return }
+
+        for service in services {
+            peripheral.discoverCharacteristics(nil, for: service)
+        }
+    }
+
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+
+        guard let characteristics = service.characteristics else {return}
+
+        for characteristic in characteristics {
+            if characteristic.uuid == CHARACTERISTIC_UUID {
+                self.characteristic = characteristic
+                self.loaded = true
+                read()
+            }
+
+        }
+    }
+
+    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
+        let data: UInt8 = characteristic.value!.withUnsafeBytes({ rawPtr in
+            return rawPtr.load(as: UInt8.self)
+        })
+
+        ledState = data == 0 ? false : true
+
+    }
+
+    func read() {
+        peripheral!.readValue(for: characteristic!)
+    }
+
+    func write(value: Bool) {
+        peripheral!.writeValue(Data([ledState ? 1 : 0]), for: characteristic!, type: .withResponse)
+    }
+}
--- Swift/AirPark/AirPark/ContentView.swift
+++ Swift/AirPark/AirPark/ContentView.swift
@@ -1,21 +1,31 @@
 //
 //  ContentView.swift
-//  AirPark
+//  Blinkytooth
 //
-//  Created by Ben Stirling on 2/16/23.
+//  Created by Ben Stirling on 2/14/23.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var led: BlinkyModel = BlinkyModel()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            if !led.loaded {
+                ProgressView()
+                
+                Text(led.connected ? "Loading..." : "Searching...")
+            } else {
+                List {
+                    Toggle("LED", isOn: $led.ledState)
+                        .onChange(of: led.ledState) { newValue in
+                            led.write(value: newValue)
+                        }
+                }
+            }
+            
         }
-        .padding()
     }
 }
 
